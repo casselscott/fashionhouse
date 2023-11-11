a42ff7e0a06f4fbdc2416bf3af7ea2e736337643
casselscott <casselscott.e@outlook.com>
2023-11-11 00:59:56 -0700

    added cart handle

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import React from 'react';
+import React, { useContext } from 'react';
+import { Store } from '../utils/Store';
 
 export default function Layout({ title, children }) {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return <>
     <Head>
       <title>{title ? title + ' - Fashion House' : 'Fashion House'}</title>
@@ -17,15 +20,22 @@ export default function Layout({ title, children }) {
             Fashion House
           </Link>
           <div>
-            <Link href="/cart" className="p-2">
-              Cart
-            </Link>
-            <Link href="/login" className="p-2">
-              Login
-            </Link>
-          </div>
-        </nav>
-      </header>
+              <Link href="/cart" legacyBehavior>
+                <a className="p-2">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </span>
+                  )}
+                </a>
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
+            </div>
+          </nav>
+        </header>
       <main className="container m-auto mt-4 px-4">{children}</main>
       <footer className="flex h-10 justify-center items-center shadow-inner">
         <p>Copyright © 2023 Fashion House</p>
