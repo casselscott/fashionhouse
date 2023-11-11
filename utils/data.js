9a2639560402bde079e72840edac281e5e3301da
casselscott <casselscott.e@outlook.com>
2023-11-11 00:25:47 -0700

    added the products

---
+++ utils/data.js
@@ -0,0 +1,69 @@
+const data = {
+    products: [
+      {
+        name: 'Free Shirt',
+        slug: 'free-shirt',
+        category: 'Shirts',
+        image: '/images/image1.jpg',
+        price: 70,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 8,
+        countInStock: 20,
+        description: 'A popular shirt',
+      },
+      
+      {
+        name: 'Slim Shirt',
+        slug: 'slim-shirt',
+        category: 'Shirts',
+        image: '/images/image3.jpg',
+        price: 90,
+        brand: 'Raymond',
+        rating: 4.5,
+        numReviews: 3,
+        countInStock: 20,
+        description: 'A popular shirt',
+      },
+     
+      {
+        name: 'Fit Pants',
+        slug: 'fit-pants',
+        category: 'Pants',
+        image: '/images/image5.jpg',
+        price: 95,
+        brand: 'Zara',
+        rating: 3.5,
+        numReviews: 7,
+        countInStock: 20,
+        description: 'A popular pants',
+      },
+      {
+        name: 'Classic Pants',
+        slug: 'classic-pants',
+        category: 'Pants',
+        image: '/images/image6.jpg',
+        price: 75,
+        brand: 'Casely',
+        rating: 2.4,
+        numReviews: 14,
+        countInStock: 20,
+        description: 'A popular pants',
+      },
+      {
+        name: 'Classic Pants',
+        slug: 'classic-pants',
+        category: 'Pants',
+        image: '/images/image7.jpg',
+        price: 75,
+        brand: 'Casely',
+        rating: 2.4,
+        numReviews: 14,
+        countInStock: 20,
+        description: 'A popular pants',
+      },
+      
+    ],
+  };
+  
+  export default data;
